ec337c9b321d38f2626225f15361444e2ec29953
Tormyst <tormyst@github.com>
2018-10-03 15:35:54 -0300

    Updating ppu for basic writing logic

--- src/gb/mem/ppu.rs
+++ src/gb/mem/ppu.rs
@@ -7,6 +7,16 @@ pub struct PPU {
 
     ly: u8, // 44
     lyc: u8, // 45
+
+    wy: u8, //4A
+    wx: u8, //4B
+}
+
+enum State {
+    HBlank,
+    VBlank,
+    Oam,
+    Vram,
 }
 
 impl PPU {
@@ -18,9 +28,12 @@ impl PPU {
             scx: 0,
             ly: 0,
             lyc: 0,
+            wy: 0,
+            wx: 0,
         }
     }
     pub fn read(&self, addr: u16) -> Option<u8> {
+        println!("Reading from PPU at {}", addr);
         match addr {
             0xFF40 => Some(self.lcdc),
             0xFF41 => Some(self.stat),
@@ -28,10 +41,32 @@ impl PPU {
             0xFF43 => Some(self.scx),
             0xFF44 => Some(self.ly),
             0xFF45 => Some(self.lyc),
+            0xFF4A => Some(self.wy),
+            0xFF4B => Some(self.wx),
             _ => None,
         }
     }
     pub fn write(&mut self, addr: u16, data: u8) -> bool {
-        false
+        println!("Writing to PPU at {} the value {}", addr, data);
+        match addr {
+            0xFF40 => {self.lcdc; true},
+            0xFF41 => {self.stat = data & 0xF8 | self.stat & 0x7; true},
+            0xFF42 => {self.scy; true},
+            0xFF43 => {self.scx; true},
+            0xFF44 => false, // LY is read only
+            0xFF45 => {self.lyc; true},
+            0xFF4A => {self.wy; true},
+            0xFF4B => {self.wx; true},
+            _ => false,
+        }
+    }
+    fn state(&self) -> State {
+        match 0x02 & self.stat {
+            0 => State::HBlank,
+            1 => State::VBlank,
+            2 => State::Oam,
+            3 => State::Vram,
+            _ => unreachable!("There are only 4 2bit states."),
+        }
     }
 }
