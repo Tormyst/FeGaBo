e9656a83b418d6068aeb98c89e392619f6be3f45
Tormyst <tormyst@github.com>
2018-10-05 12:32:32 -0300

    Making the none cartrage not fail on read
    Instead, reads are now zero.  Writes still fail for now.

--- src/gb/mem/cm/mod.rs
+++ src/gb/mem/cm/mod.rs
@@ -9,7 +9,7 @@ pub struct NoneCartrageMapper {}
 
 impl CartrageMapper for NoneCartrageMapper {
     fn read(&self, addr: u16) -> Option<u8> {
-        None
+        Some(0x00)
     }
     fn write(&mut self, addr: u16, data: u8) -> bool {
         false
